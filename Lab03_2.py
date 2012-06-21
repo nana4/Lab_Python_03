a6949a07c195aeafd7d015e490754ffefb2024c7
AITI STUDENT <aiti-student@aiti-092-PC.(none)>
2012-06-21 16:55:39 +0000

    lab 03 uploads

---
+++ Lab03_2.py
@@ -0,0 +1,17 @@
+#Lab03_2a
+for i in range(0,8,1):
+    for j in range(1,i,1):
+        print j,
+    i-=1
+    print""
+    
+print "-----------------------------"
+#Lab03_2b
+for i in range(7,0,-1):
+    for j in range(1,i,1):
+        print j,
+    i-=1
+    print""
+
+
+print "-----------------------------"
---
+++ lab03_1.py
@@ -0,0 +1,20 @@
+i=0
+num=2
+print "why?"
+while i<50:
+    j=1
+    while j<num:
+        if (num%j)!=0:
+            j+=j
+    print num            
+    num+=num
+    i+=i
+      ### """ factor=num/j
+        ###if (num/j)==num:
+           ### j+=j
+     ###   print num
+        ###num+=num
+  ###  i+=i"""
+       
+        
+    
